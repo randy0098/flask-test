8aeb7fdbf1292aede84ab2926c62feb7001f7da7
randy0098 <lyon0098@163.com>
2019-02-21 11:29:03 +0800

    添加Flask-Migrate。

--- flask_test.py
+++ flask_test.py
@@ -1,10 +1,10 @@
 import os
-# from flask_migrate import Migrate
+from flask_migrate import Migrate
 from app import create_app, db
-# from app.models import User, Role
+from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-# migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 # @app.shell_context_processor
 # def make_shell_context():
